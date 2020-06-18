34c4a40aa861f2f1b4a7f7f13f24cfd1a75b8666
cedricchaffard <cedric.chaffard@gmail.com>
2020-06-18 20:46:31 +0200

    Initial commit of files MOBILE FIRST

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+
+const cercle = document.querySelector('.cercle');
+
+const menu = document.querySelector('.menug');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+cercle.addEventListener('click', () => {
+    cercle.classList.toggle('active');
+
+});
